1d580fbf01a108abc1a0af08874a0da3f2f65a2d
mth830 <mth830@gmail.com>
2025-08-21 18:10:51 -0400

    updated the position calculations for dragging

--- planner/src/components/PlanningBoard.tsx
+++ planner/src/components/PlanningBoard.tsx
@@ -23,6 +23,7 @@ const defaultEventItem: EventItem = {
 };
 export const PlanningBoard = () => {
   const [eventItem, setEventItem] = useState<EventItem>(defaultEventItem);
+
   const updateUserPosition = (x: number, y: number, userId: number) => {
     const isDeployed = eventItem.deployedUsers.some(
       (user) => user.id === userId
@@ -30,31 +31,47 @@ export const PlanningBoard = () => {
     //if(!isDeployed){;} move this to move people from available to deployed if they are not deployed
     //check to see if the coordinates left out of the bounding box of the container
     const { deployedUsers } = eventItem;
+
     const deployedUsersClone: User[] = JSON.parse(
       JSON.stringify(deployedUsers)
     );
+
     const selectedUser = deployedUsersClone.find((user) => user.id === userId);
+
     if (!selectedUser) {
       console.warn("User not found with selected ID");
       return;
     }
-    selectedUser.x = x - 500; //this is another part of the issue
-    selectedUser.y = y - 500; //this is part of the issue
+
+    const pb = document.getElementById("PlanningBoard");
+    const pbBoundingRectangle:DOMRect = pb.getBoundingClientRect();
+
+    if(!pb){
+      console.warn("error finding bounding box for planningBoard")
+      return;
+    }
+    const offsetX = pbBoundingRectangle?.x;
+    const offsetY = pbBoundingRectangle?.y;
+    console.log(pbBoundingRectangle)
+    selectedUser.x = x - offsetX; //this is another part of the issue
+    selectedUser.y = y - offsetY; //this is part of the issue
     setEventItem({ ...eventItem, deployedUsers: [...deployedUsersClone] });
   };
   const handleDrag = (event: React.DragEvent) => {
-    const { screenX, screenY } = event;
+
+    const { clientX, clientY } = event;
+    console.log(event.targetg)
     const id = event.target.id;
 
-    updateUserPosition(screenX, screenY, +id);
+    updateUserPosition(clientX, clientY, +id);
   };
   return (
-    <>
+    <div id="PlanningBoard" style={{minHeight:500,minWidth:500}}>
       <div >{eventItem.title}</div>
       <div style={{ position: "absolute" }}>
-        {eventItem.deployedUsers.map(({ name, x, y, id }, i) => (
+        {eventItem.deployedUsers.map(({ name, x, y, id }) => (
           <div
-            key={id + i}
+            key={id}
             draggable={true}
             onDragEnd={handleDrag}
             id={id.toString()}
@@ -71,6 +88,6 @@ export const PlanningBoard = () => {
           </div>
         ))}
       </div>
-    </>
+    </div>
   );
 };
